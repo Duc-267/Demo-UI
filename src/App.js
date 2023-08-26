dbb78ad9c0973cac750677ab7d70af8fcc9f4405
Nam Hoang <namhoang@b13technology.com>
2023-08-27 00:01:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Popover } from "react-text-selection-popover";
+import { css } from '@emotion/css'
+import audio from './audio.png'
+import marker from './marker.png'
+import magnifyingGlass from './magnifying-glass.png'
 
 function App() {
+  const [text, setText] = React.useState('')
+  const [isQuickSearch, setIsQuickSearch] = React.useState(false)
+  const [quickSearchText, setQuickSearchText] = React.useState('')
+
+  function handleTextChange(text) {
+    setText(text)
+  }
+
+  const escapeRegExp = (str = '') => (
+    str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')
+  );
+
+  const Highlighted = ({text = '', highlight = ''}) => {
+    if (!highlight.trim()) {
+      return <span>{text}</span>
+    }
+    const regex = new RegExp(`(${escapeRegExp(highlight)})`, 'gi')
+    const parts = text.split(regex)
+    return (
+      <span>
+         {parts.filter(part => part).map((part, i) => (
+             regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>
+         ))}
+     </span>
+    )
+ }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Container'>
+      <Highlighted text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." highlight={text} />
+      <Popover
+        render={
+          ({ clientRect, isCollapsed, textContent }) => {
+            if (isCollapsed) {
+              setIsQuickSearch(false)
+            }
+            if (clientRect == null || isCollapsed) return null
+            const style = css`
+              position: absolute;
+              left: ${clientRect.left + clientRect.width / 2}px;
+              top: ${clientRect.top - 40}px;
+              margin-left: -75px;
+              background: gray;
+              font-size: 14px;
+              text-align: center;
+              box-shadow: black 0px 0px 20px;
+              border-radius: 3px;
+              font-size: 14px;
+              max-width: 200px;
+              z-index: 22147483647;
+              color: rgb(15, 33, 73);
+              opacity: 0.97;
+              background-color: white;
+              line-height: 1.5em;
+              vertical-align: baseline;
+              text-align: left;
+              box-shadow: black 0px 0px 20px;
+              min-width: 90px;
+              border-radius: 5px;
+              width: auto;
+            `
+            const iconButtonStyle = css`
+              background: none;
+              width: 20px;
+              height: 20px;
+              border: none;
+              cursor: pointer;
+              border-radius: 5px;
+              padding: 10px;
+              :hover {
+                background: #E5E5E5;
+              }
+            `
+            if (isQuickSearch) {
+              return (
+                <div className={style}>
+                  <div style={
+                    {
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      paddingLeft: '10px',
+                      paddingRight: '10px'
+                    }
+                  }>
+                    <p style={
+                      {
+                        fontWeight: 'bold',
+                        fontSize: '16px',
+                        marginBottom: '10px'
+                      }
+                    }>{quickSearchText}</p>
+                    <img onClick={() => {console.log('pronoun')}} src={audio} alt="audio" style={
+                      {
+                        width: '20px',
+                        height: '20px',
+                        cursor: 'pointer'
+                      }
+                    } />
+                  </div>
+                  <p style={
+                    {
+                      fontSize: '14px',
+                      marginBottom: '10px',
+                      color: '#2F2D36',
+                      paddingLeft: '10px',
+                      paddingRight: '10px'
+                    }
+                  }>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut .
+                  </p>
+                </div>
+              )
+
+            }
+            return <div className={style}>
+              <img className={iconButtonStyle} src={marker} onClick={() => {handleTextChange(textContent)}}/>
+
+              <img 
+              src={magnifyingGlass}
+              className={iconButtonStyle}
+              onClick={() => {
+                setIsQuickSearch(true)
+                setQuickSearchText(textContent)
+              }}/>
+            </div>
+          }
+        }
+      />
+      </div>
     </div>
   );
 }
